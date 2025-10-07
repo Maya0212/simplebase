5b958d31ada09a9cba4ca1a9a643f584e6ac934c
Maya0212 <mk233@hawaii.edu>
2025-10-07 11:33:53 -1000

    fixed drawer  nav animatiobnbug

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     
-// GLOBAL FUNCTION GO HERE
+// ENABLE MOBILE MENU
+const sitenav = document.querySelector(".site-nav");
+const menubutton = document.querySelector(".menubutton")
+
+menubutton.onclick = () => {
+  
+//   IF MENU US CLOSED, OPEN IT, ELSE CLOSE IT
+  if (sitenav.getAttribute("data-menustate") === "open" ) {
+    sitenav.setAttribute("data-menustate", "closed");
+  } else {
+    sitenav.setAttribute("data-menustate", "open");
+  };
+  
+};
 
 });
+
+// Removing the data-attribute on resize to fix darawer nav animation
+window.onresize = () => {
+  sitenav.removeAttribute("data-menustate");
+};
+
